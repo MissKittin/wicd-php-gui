93ab4ff5f66c05a3935533d42e636f8ea561c5e9
MissKittin <you@example.com>
2019-10-30 16:30:19 +0000

    modified:   setup.sh 	modified:   wicd-config-injection.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,47 @@
 #!/bin/sh
+echo 'rm README.md'
 rm README.md
+
+echo 'rm LICENSE'
 rm LICENSE
+
+echo 'rm *.png'
 rm bright_preview.png
 rm default_preview.png
+
+echo 'shell.sh'
 chmod 755 shell.sh
+
+echo 'wicd-config-injection.sh'
 chmod 755 wicd-config-injection.sh
 
-cat << EOF > download-jquery.sh
-#!/bin/sh
-wget https://code.jquery.com/jquery-3.3.1.min.js
-rm download-jquery.sh
-exit 0
-EOF
+echo; echo -n 'download jquery? [y/n] '
+read answer
+while true; do
+	jqueryerror=false
+	if [ "$answer" = 'y' ]; then
+		echo; echo 'jquery-3.3.1.min.js'
+		wget https://code.jquery.com/jquery-3.3.1.min.js > /dev/null 2>&1 || jqueryerror=true
+		if $jqueryerror; then
+			echo; echo -n 'download error, create download script? [y/n] '
+			read answerb
+			if [ "$answerb" = 'y' ]; then
+				echo '#!/bin/sh' > download-jquery.sh
+				echo 'error=false' >> download-jquery.sh
+				echo 'echo "jquery-3.3.1.min.js"' >> download-jquery.sh
+				echo 'wget https://code.jquery.com/jquery-3.3.1.min.js > /dev/null 2>&1 || error=true' >> download-jquery.sh
+				echo '$error || rm download-jquery.sh' >> download-jquery.sh
+				echo 'exit 0' >> download-jquery.sh
+				chmod 755 download-jquery.sh
+				break
+			fi
+			[ "$answerb" = 'n' ] && break
+		fi
+		break
+	fi
+	[ "$answer" = 'n' ] && break
+done
 
-chmod 755 download-jquery.sh
-echo 'Run download-jquery.sh'
+echo; echo 'setup.sh'
 rm setup.sh
 exit 0
